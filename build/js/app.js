fe22273b8d3f5ddb61dea0723c5c933d34fec634
Levi Bibo and Luke Philips <luke@>
2017-01-03 14:01:08 -0800

    add snooze and second alarm function

--- build/js/app.js
+++ build/js/app.js
@@ -21,12 +21,21 @@ var Alarm = require('./../js/alarm.js').alarmModule;
 $(document).ready(function() {
   var now = moment();
   var userAlarm = null;
+  var userAlarm2 = null;
+
 
   $(".alarm-form").submit(function(event) {
     event.preventDefault();
     var alarmTimeInput = $("#user-input-time").val();
     userAlarm = new Alarm(moment(alarmTimeInput,'HH:mm'));
     $('.alarm').text(alarmTimeInput);
+  });
+
+  $(".alarm-form-2").submit(function(event) {
+    event.preventDefault();
+    var alarmTimeInput2 = $("#user-input-time-2").val();
+    userAlarm2 = new Alarm(moment(alarmTimeInput2,'HH:mm'));
+    $('.alarm-2').text(alarmTimeInput2);
   });
 
   currentTime = now.format("HH:mm");
@@ -43,11 +52,25 @@ $(document).ready(function() {
       $(".alarm-alert").show();
       $('.alarm').css("color", "darkred");
     }
-  }, 10000);
+    if (userAlarm2 !== null && now >= userAlarm2.alarmTime && !userAlarm2.hasAlerted){
+      userAlarm2.hasAlerted = true;
+      $('.alarm-image').attr("src", userAlarm2.randomAlert());
+      $(".alarm-alert").show();
+      $('.alarm').css("color", "darkred");
+    }
+  }, 5000);
 
   $("#disable").click(function(){
     $(".alarm-alert").hide();
     $('.alarm').css("color", "darkblue");
+  });
+
+  $("#snooze").click(function(){
+    $(".alarm-alert").hide();
+    setTimeout(function(){
+      $(".alarm-alert").show();
+      $('.alarm-image').attr("src", userAlarm.randomAlert());
+    }, 5000);
   });
 });
 
